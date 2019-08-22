f0bb48d288a405b226885f5352ac1b51b000e038
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2019-08-22 15:00:33 +0530

    add file Main.java

---
+++ src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+/**
+ * @author Sajal
+ * @version 1.0
+ * */
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+    Scanner scanner = new Scanner(System.in);
+    String name=scanner.nextLine();
+    System.out.println("Welcome, "+name+"!");
+}
+}
