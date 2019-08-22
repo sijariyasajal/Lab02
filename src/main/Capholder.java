983ceb7b9e7837a1399407e91ab107189c62667c
manishgautam30 <manish.gautam_cs18@gla.ac.in>
2019-08-22 15:57:32 +0530

    assignment completed

---
+++ src/main/Capholder.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.Scanner;
+
+public class Capholder {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        double time = obj.nextDouble();
+        double v;
+        double s;
+        v = 9.8 * time;
+        s = 0.5 * 9.8 * time * time;
+        System.out.println("The speed of the object at 6.0 seconds after its release is " + v + " and the distance the object has travelled in the 6.0 seconds after the relase is " + s);
+
+
+    }
+}
