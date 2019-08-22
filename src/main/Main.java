fc0fd057dce0bdc712dd0369089e6c80e67751fa
sijariyasajal <53899486+sijariyasajal@users.noreply.github.com>
2019-08-22 15:31:55 +0530

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -3,13 +3,14 @@ package main;
 /**
  * @author Sajal
  * @version 1.0
- * */
+ */
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine().trim();
     System.out.println("Welcome, "+name+"!");
+    System.out.println("ho gaya");
     Scanner.close();
 
     }
